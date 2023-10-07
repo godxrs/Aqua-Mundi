e098fc49a3b324bc5ceb0b2ff95e96840d0dba8a
Ramon Garcia <ramon.garcia08@utrgv.edu>
2023-10-07 15:56:47 -0500

    working on home screen

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./assets/background.jpg')}
+      style={styles.container}
+    >
+      <LinearGradient
+        colors={['#052D5264', '#39934850']}
+        style={{ height: '100%', width: '100%' }}>
+
+        <View style={styles.inner_container}>
+          <Text style={styles.text_lg}>App Name</Text>
+          <Text style={styles.text_md}>
+            app slogan ,saying,
+            blah  brief description,
+            hello yes welcome?
+            blahanything
+          </Text>
+          <View></View>
+        </View>
+
+      </LinearGradient>
+    </ImageBackground>
   );
 }
 
@@ -17,4 +36,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text_lg: {
+    fontSize: 80,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    textAlign: 'left'
+  },
+  text_md: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    textAlign: 'right'
+  },
+  inner_container: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    justifyContent: 'center',
+    paddingHorizontal: 40,
+    justifyContent: 'space-around'
+  }
 });
--- main.py
+++ main.py
@@ -26,14 +26,15 @@ def request(url):
         print("Error requesting API")
 
 def main():
-    county_name = "Hidalgo"
-    counties = request_api(f"https://cbdnet.info/arcgis/rest/services/Hosted/2023_SpeciesByCounty/FeatureServer/0/query?where=Name%3D%27{county_name}%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&defaultSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false&returnCentroid=false&timeReferenceUnknownClient=false&maxRecordCountFactor=&sqlFormat=none&resultType=&datumTransformation=&lodType=geohash&lod=&lodSR=&f=pjson")['features']
+    state= "Maine"
+    county_name = "Washington"
+    counties = request_api(f"https://cbdnet.info/arcgis/rest/services/Hosted/2023_SpeciesByCounty/FeatureServer/0/query?where=Name%3D%27{county_name}%27+AND+State%3D%27{state}%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&defaultSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false&returnCentroid=false&timeReferenceUnknownClient=false&maxRecordCountFactor=&sqlFormat=none&resultType=&datumTransformation=&lodType=geohash&lod=&lodSR=&f=pjson")['features']
 
     for county in counties:        
         species = request_api(f"https://cbdnet.info/arcgis/rest/services/Hosted/2023_SpeciesByCounty/FeatureServer/1/query?where=st_co%3D%27{county['attributes']['st_co']}%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&defaultSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false&returnCentroid=false&timeReferenceUnknownClient=false&maxRecordCountFactor=&sqlFormat=none&resultType=&datumTransformation=&lodType=geohash&lod=&lodSR=&f=pjson")['features']
         for specie in species:
             if isMarineLife(specie['attributes']['sciname']):
-                print(specie['attributes']['comname'])
+                print(specie['attributes']['comname'] + " : " + specie['attributes']['sciname'])
             else:
                 print("NOT : " + specie['attributes']['comname'])
 
